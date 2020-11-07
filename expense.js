13907ae859a92756f1c928d8feca7ca37384c0bc
daalimouhcine <daalim277@gmail.com>
2020-11-07 15:04:14 +0100

    Add files via upload

--- expense.js
+++ expense.js
@@ -8,6 +8,7 @@ const tbody = document.querySelector(".elements");
 
 
 //Creat EVENTS
+document.addEventListener("DOMcontentLoaded",returnStorage)
 submitButton.addEventListener("click",addElement);
 
 
@@ -30,17 +31,21 @@ function addElement (event) {
             tAmount.innerText = amount.value;
             tDelete.innerHTML = '<button class="delete-btn"><i class="fas fa-trash"></i></button>';
 
+            
         //append the elements
         tbody.appendChild(trow);
         trow.appendChild(tName);
         trow.appendChild(tDate);
         trow.appendChild(tAmount);
         trow.appendChild(tDelete);
+
+        saveLocal(name.value,date.value,amount.value);
             
         name.value = "";
         date.value = "";
         amount.value = "";
-        
+
+
         //Check if the table is empty
         check();
 
@@ -52,9 +57,11 @@ function addElement (event) {
 }
 
 function removeElement(e) {
-    const parent = e.target.parentNode.parentNode.parentNode;
+    const parent = e.target.parentNode.parentNode;
 
-    parent.style.display = "none";
+    if(e.target.classList[0] === "delete-btn"){
+        parent.remove();
+    }
 
     //Check if the table is empty
     check();
@@ -70,6 +77,61 @@ function check() {
     }
 }
 
-function saveLocal() {
+function saveLocal(itemName,itemDate,itemAmount) {
+    let nameItem;
+    let dateItem;
+    let amountItem;
+    if(localStorage.getItem("nameItem") === null && localStorage.getItem("dateItem") === null && localStorage.getItem("amountItem") === null) {
+        nameItem = [];
+        dateItem = [];
+        amountItem = [];
+    } else {
+        nameItem = JSON.parse(localStorage.getItem("nameItem"));
+        dateItem = JSON.parse(localStorage.getItem("dateItem"));
+        amountItem = JSON.parse(localStorage.getItem("amountItem"));
+    }
 
+    nameItem.push(itemName);
+    dateItem.push(itemDate);
+    amountItem.push(itemAmount);
+    
+    localStorage.setItem("nameItem",JSON.stringify(nameItem));
+    localStorage.setItem("dateItem",JSON.stringify(dateItem));
+    localStorage.setItem("amountItem",JSON.stringify(amountItem));
+}
+
+function returnStorage() {
+
+    let nameItem;
+    let dateItem;
+    let amountItem;
+    if(localStorage.getItem("nameItem") === null && localStorage.getItem("dateItem") === null && localStorage.getItem("amountItem") === null) {
+        nameItem = [];
+        dateItem = [];
+        amountItem = [];
+    } else {
+        nameItem = JSON.parse(localStorage.getItem("nameItem"));
+        dateItem = JSON.parse(localStorage.getItem("dateItem"));
+        amountItem = JSON.parse(localStorage.getItem("amountItem"));
+    }
+
+
+    nameItem.forEach(function(item) {
+
+        //Creat element in table
+        const trow = document.createElement("tr");
+        const tName = document.createElement("td");
+
+            //Add the content
+            tName.innerText = nameItem[item];
+            tDelete.innerHTML = '<button class="delete-btn"><i class="fas fa-trash"></i></button>';
+
+            
+        //append the elements
+        tbody.appendChild(trow);
+        trow.appendChild(tName);
+
+
+
+    });
 }
